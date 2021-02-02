457551245fe9ede70dfb7c11c6869254ddfb0476
SirSanewa <lukaszsanewski@gmail.com>
2021-02-02 18:18:57 +0100

    small change

--- 1. Basics/plot_graph.py
+++ 1. Basics/plot_graph.py
@@ -1,8 +1,18 @@
 import matplotlib.pyplot as plt
 
+
 x = [i for i in range(10)]
 y = [i*2 for i in x]
 
 # create plot graph with x and y lists
-plt.plot(x, y)
+# plt.plot(x, y)
+
+# create scattered points graph with x and y lists
+plt.scatter(x, y)
+
+# label names
+plt.xlabel("x-axis")
+plt.ylabel("y-axis")
+
+# show graph
 plt.show()
