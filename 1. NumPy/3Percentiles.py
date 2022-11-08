13e7c2de79b7822fbecc5ddfe31954d80ac5a3e1
SirSanewa <lukaszsanewski@gmail.com>
2022-11-08 11:47:39 +0100

    new_course

--- 1. NumPy/3Percentiles.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-ages = [5, 31, 43, 48, 50, 41, 7, 11, 15, 39, 80, 82, 32, 2, 8, 6, 25, 36, 27, 61, 31]
-
-# Percentyle - wywołuje się podając zakres danych oraz procent wartości które mają być mniejsze lub równe
-ages_percinteles = np.percentile(ages, 75)
-print("Percentyl -", ages_percinteles)
-# Oznacza że 75% liczb(z argumentu) jest mniejsze bądź równe 43.0
--- 1. NumPy/5Histogram.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-x = np.random.uniform(0.0, 5.0, 100)
-
-plt.hist(x, 5) # liczba wykresów słupkowych
-plt.show()
-# pokazuje, że między 0 a 1 jest 14 wartości
